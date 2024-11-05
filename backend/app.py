7a1b21db9be79dd4e26ca51163d6a56efec5e09f
Codindex <61005232+Codindex@users.noreply.github.com>
2024-11-05 13:03:56 +0100

    Update app.py
    Add 4 routes
    - addition
    - soustraction
    - multiplication
    - division

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello Cluster admins'
+
+@app.route('/add')
+def add():
+    # return str(3 + 4)
+    return jsonify({"result": str(3+4)})
+
+@app.route('/api/addition')
+def get_addition():
+    a = int(request.args.get('a', 0))
+    b = int(request.args.get('b', 0))
+    result = a + b
+    return jsonify({'result': result})
+
+@app.route('/api/soustraction')
+def get_substraction():
+    a = int(request.args.get('a', 0))
+    b = int(request.args.get('b', 0))
+    result = a - b
+    return jsonify({'result': result})
+
+@app.route('/api/multiplication')
+def get_multiplication():
+    a = int(request.args.get('a', 0))
+    b = int(request.args.get('b', 0))
+    result = a * b
+    return jsonify({'result': result})
+
+@app.route('/api/division')
+def get_division():
+    a = int(request.args.get('a', 0))
+    b = int(request.args.get('b', 0))
+    result = a / b
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
