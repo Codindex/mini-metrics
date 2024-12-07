5080efaf6f770f8b7114cc2b546c64e88eaace50
Codindex <61005232+Codindex@users.noreply.github.com>
2024-12-07 14:28:52 +0100

    Create auth routes
    Create signup and login routes for authentication process

---
+++ frontend/src/app/api/login/route.ts
@@ -0,0 +1,63 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/lib/db/client";
+import { SigninFormSchema } from "@/lib/definitions";
+import { createSession, deleteSession, getUserId } from "@/lib/session";
+import type { SigninInput } from "@/lib/form/definitions";
+
+export async function POST(request: Request) {
+  const loginInput: SigninInput = await request.json();
+
+  // Validation
+  const validatedFields = SigninFormSchema.safeParse(loginInput);
+
+  if (!validatedFields.success) {
+    return Response.json({
+      errors: validatedFields.error.flatten().fieldErrors,
+    }, {
+      status: 400,
+    });
+  }
+
+  const { email, password } = validatedFields.data;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: email,
+    }
+  })
+
+  if (!user) {
+    return new Response("No user found with this email", {
+      status: 400,
+    });
+  }
+
+  const passed = await bcrypt.compare(password, user.password);
+  console.log(passed);
+
+  if (!passed) {
+    return new Response("Wrong credentials, try again", {
+      status: 403,
+    });
+  }
+
+  await createSession(user.id.toString());
+
+  return Response.json(user, {
+    status: 200,
+  })
+}
+
+export async function DELETE() {
+  const userId = await getUserId();
+  if (!userId) return new Response("You're not logged in", {
+    status: 403,
+  });
+
+  deleteSession();
+
+  return new Response("Log out done", {
+    status: 200,
+  });
+}
---
+++ frontend/src/app/api/signup/route.ts
@@ -0,0 +1,45 @@
+import bcrypt from "bcrypt";
+
+import prisma from "@/lib/db/client";
+import { SignupFormSchema } from "@/lib/definitions";
+import { createSession } from "@/lib/session";
+import type { SignupInput } from "@/lib/form/definitions";
+
+export async function POST(request: Request) {
+  const signupForm: SignupInput = await request.json();
+
+  // Validation
+  const validatedFields = SignupFormSchema.safeParse(signupForm);
+
+  if (!validatedFields.success) {
+    return Response.json({
+      errors: validatedFields.error.flatten().fieldErrors,
+    }, {
+      status: 400,
+    });
+  }
+
+  // Auth
+  const hashedPassword = await bcrypt.hash(signupForm.password, 10)
+
+  const user = await prisma.user.create({
+    data: {
+      name: signupForm.username,
+      email: signupForm.email,
+      password: hashedPassword
+    },
+  })
+
+  if (!user) {
+    return {
+      message: "An error occured while creating your account."
+    }
+  }
+
+  // Session
+  await createSession(user.id.toString())
+
+  return Response.json(user, {
+    status: 200,
+  });
+}
