166820c1d1725887ef257c2a9d3ad87c16e65f30
Codindex <61005232+Codindex@users.noreply.github.com>
2024-12-17 18:25:02 +0100

    Update RequestFormula type
    Mark optional fields for new formulas

--- frontend/src/lib/api/types.ts
+++ frontend/src/lib/api/types.ts
@@ -1,4 +1,4 @@
 import { Formula, Result } from "@prisma/client";
 
-export type RequestFormula = Omit<Formula, "id" | "authorId" | "createdAt">;
+export type RequestFormula = Pick<Formula, "formula"> & Partial<Omit<Formula, "id" | "formula" | "authorId" | "createdAt">>;
 export type RequestResult = Omit<Result, "id" | "formulaId" | "createdAt">;
